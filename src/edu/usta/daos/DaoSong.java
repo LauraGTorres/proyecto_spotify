c85b3ab178b37606a92283391e11acd4d97b3431
laura <laura@ComputadorLaura>
2025-05-28 17:37:28 -0500

    Daos song

--- src/edu/usta/daos/DaoSong.java
+++ src/edu/usta/daos/DaoSong.java
@@ -3,13 +3,17 @@ package edu.usta.daos;
 
 
 import edu.usta.database.Conexion;
+import edu.usta.domain.Artist;
+import edu.usta.domain.Genere;
 import edu.usta.domain.Song;
 import edu.usta.interfaces.FuncionalidadCanArt;
+import java.math.BigDecimal;
 import java.util.List;
 import java.sql.ResultSet;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.logging.Logger;
 import java.util.logging.Level;
 
@@ -38,7 +42,32 @@ public class DaoSong extends Conexion implements FuncionalidadCanArt<Song> {
 //Connection conn = MiConexion.getInstancia().getConexion();
     @Override
     public List<Song> consultar(String orden) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try {
+            if (orden.isEmpty()) {
+                orden = "idCancion"; 
+            }
+            miCadenaSQL = "SELECT idCancion, idGenero, idArtista, tituloCancion, duracionCancion, archivoCancion From cancion ORDER BY"
+                    + orden; 
+             miConsulta = miObjetoConexion.prepareStatement(miCadenaSQL); 
+             misRegistros = miConsulta.executeQuery(); 
+             
+             List<Song> arrySong = new ArrayList<>(); 
+             while (misRegistros.next()) {
+                 int idSon = misRegistros.getInt(1); 
+                 int idGen = misRegistros.getInt(2);
+                 int idArtis = misRegistros.getInt(3); 
+                 String tituloSon = misRegistros.getString(4); 
+                 BigDecimal duraSon = misRegistros.getBigDecimal(5); 
+                 Byte archiSong = misRegistros.getByte(6); 
+                 
+                 Artist obArtist = new Artist(); 
+                 
+                
+            }
+        } catch (Exception e) {
+            
+        }
+        return null; 
     }
 
     @Override
